92ed1d1617a3c456ba1c7be60c6fc50059d25554
krchandler27 <krchandler27@gmail.com>
2022-11-21 18:49:02 -0700

    starter javacode for password generator, generates 4 digit pin rn

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
-// Assignment code here
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
+function getPasswordCriteria() {
+  var userChoseNumbers = confirm("Do you want numbers in your password?");
+  var options = {
+    userChoseNumbers: userChoseNumbers
+  };
+  return options;
+}
+
+function createRandom(length) {
+  var randomNum = Math.floor(Math.random() * length);
+  return randomNum;
+}
+
+function generatePassword() {
+  var userChose = getPasswordCriteria();
+  var availableChars = [];
+  var passwordArr = [];
+  if (userChose.userChoseNumbers) {
+    availableChars = availableChars.concat(numbers);
+  }
+
+  for (var i = 0; i < 4; i++) {
+    passwordArr.push(availableChars[createRandom(availableChars.length)])
+  }
+
+
+  return passwordArr.join("")
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
