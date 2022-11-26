bffc48d238349797dc8691431d96a4fea737d683
krchandler27 <krchandler27@gmail.com>
2022-11-26 14:32:56 -0700

    I made sure there was an array for each needed list i.e. all numbers, lowercase letters, uppercase letters, and special characters. I also updated each of the confirm prompts and fixed typos.

--- script.js
+++ script.js
@@ -1,43 +1,48 @@
+// List of possible password characters
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var letters = ["a", "b", "c", "d", "e", "f"]
+var lowLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var capLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var specialChars = ["!", "@", "#", "$", "%", "^", "&", "*", "=", "-", "_"];
 
 function generatePassword() {
-  // var userChose = createPasswordCriteria();
-  var userChoseNumbers = confirm("Do you want numbers in your password?");
-  var userChoseLetters = confirm("Do you want letters in your password?");
+  // var userChoosePasswordLength = 
+  var userChoseNumbers = confirm("Select 'OK' if you would you like to include numbers in your password. If not, select 'Cancel'.");
+  var userChoseLowLetters = confirm("Select 'OK' if you would you like to include lowercase letters in your password. If not, select 'Cancel'.");
+  var userChoseCapLetters = confirm("Select 'OK' if you would you like to include uppercase letters in your password. If not, select 'Cancel'.");
+  var userChoseSpecialChars = confirm("Select 'OK' if you would you like to include special characters in your password. If not, select 'Cancel'.");
   var availableChars = [];
   var passwordArr = [];
+
   if (userChoseNumbers) {
     availableChars = availableChars.concat(numbers);
   }
-
-  if (userChoseLetters) {
-    availableChars = availableChars.concat(letters);
+  if (userChoseLowLetters) {
+    availableChars = availableChars.concat(lowLetters);
   }
- 
-  for (var i = 0; i < 8; i++) {
-    var randomNum = Math.floor(Math.random() * availableChars.length);
-    var randomChar = availableChars[randomNum];
-    passwordArr.push(randomChar);
+  if (userChoseCapLetters) {
+    availableChars = availableChars.concat(capLetters);
+  }
+  if (userChoseSpecialChars) {
+    availableChars = availableChars.concat(specialChars);
   }
 
+
+    for (var i = 0; i < 25; i++) {
+      var randomNum = Math.floor(Math.random() * availableChars.length);
+      var randomChar = availableChars[randomNum];
+      passwordArr.push(randomChar);
+    }
 
   return passwordArr.join("");
 }
-// When we click on the button we want password to show on the screen
-// Grab the #generate element or generate button
+
 var generateBtn = document.querySelector("#generate");
 
-
-// Write password to the #password input
 function writePassword() {
-  // var password = generatePassword();
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
