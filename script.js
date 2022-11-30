27f651fd29c578b541eb33dc4c0dafb3f26480bf
krchandler27 <krchandler27@gmail.com>
2022-11-29 19:17:01 -0700

    added some code to provide another prompt to control correct password length

--- script.js
+++ script.js
@@ -4,8 +4,15 @@ var lowLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m
 var capLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var specialChars = ["!", "@", "#", "$", "%", "^", "&", "*", "=", "-", "_"];
 
+
+
 function generatePassword() {
-  var userChoosePasswordLength = prompt("Choose the desired length of your password between 8-128 characters.")
+  var userChoosePasswordLength = prompt ("Please choose the desired length of your password (this must be between 8-128 characters).")
+
+  if (userChoosePasswordLength < 8 || userChoosePasswordLength > 128) {
+    var userChoosePasswordLength2 = prompt ("You chose " + userChoosePasswordLength + " characters for your password. This is not acceptable criteria for this number generator. Please choose a password length between 8-128 characters.");
+  }
+
   var userChoseNumbers = confirm("Select 'OK' if you would you like to include numbers in your password. If not, select 'Cancel'.");
   var userChoseLowLetters = confirm("Select 'OK' if you would you like to include lowercase letters in your password. If not, select 'Cancel'.");
   var userChoseCapLetters = confirm("Select 'OK' if you would you like to include uppercase letters in your password. If not, select 'Cancel'.");
@@ -13,10 +20,8 @@ function generatePassword() {
   var availableChars = [];
   var passwordArr = [];
 
-
-  if (userChoosePasswordLength < 8 || userChoosePasswordLength > 128) {
-    
-  }
+ 
+  
   
   if (userChoseNumbers) {
     availableChars = availableChars.concat(numbers);
